9060b37783f7b0f411047a7bf8c474e4053aa87c
Vadim <33183684+bychkovvadim@users.noreply.github.com>
2019-03-01 09:30:10 +0300

    Подготовка к предзащите

---
+++ js/script.js
@@ -0,0 +1,55 @@
+var map;
+var searchBtn = document.querySelector(".search-btn");
+var searchForm = document.querySelector(".search-form");
+var checkInDate = searchForm.querySelector("[name=check-in-date]");
+var buttonPlusAduits = searchForm.querySelector(".aduits-visitors .search-btn-plus");
+var buttonMinAduits = searchForm.querySelector(".aduits-visitors .search-btn-min");
+var plusAduits = document.getElementById('aduits-visitors');
+var minAduits = document.getElementById('aduits-visitors');
+var buttonPlusChildren = searchForm.querySelector(".children-visitors .search-btn-plus");
+var buttonMinChildren = searchForm.querySelector(".children-visitors .search-btn-min");
+var plusChildrens = document.getElementById('children-visitors');
+var minChildrens = document.getElementById('children-visitors');
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 34.871002, lng: -111.760826},
+    zoom: 9,
+    disableDefaultUI: true
+  });
+}
+
+searchBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchForm.classList.toggle("form-show");
+  checkInDate.focus();
+});
+
+
+buttonPlusAduits.addEventListener("click", function (evt) {
+  if (!plusAduits.value > 1 || plusAduits.value < 10) {
+  evt.preventDefault();
+  plusAduits.value=parseInt(document.getElementById('aduits-visitors').value)+1;
+  }
+});
+
+buttonMinAduits.addEventListener("click", function (evt) {
+  if (minAduits.value > 1) {
+  evt.preventDefault();
+  minAduits.value=parseInt(document.getElementById('aduits-visitors').value)-1;
+  }
+});
+
+buttonPlusChildren.addEventListener("click", function (evt) {
+  if (! plusChildrens.value > 0 || plusChildrens.value < 10) {
+  evt.preventDefault();
+  plusChildrens.value=parseInt(document.getElementById('children-visitors').value)+1;
+  }
+});
+
+buttonMinChildren.addEventListener("click", function (evt) {
+  if (minChildrens.value > 0) {
+  evt.preventDefault();
+  minChildrens.value=parseInt(document.getElementById('children-visitors').value)-1;
+  }
+});
